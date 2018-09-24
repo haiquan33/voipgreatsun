eb31255b0c6e1596152f1ddbd8327c3d25582258
haiquan33 <haiquan331996@gmail.com>
2018-09-24 09:52:08 +0700

    add_save_user_phonenumber

--- components/LoginComponents/LoginScreen.js
+++ components/LoginComponents/LoginScreen.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TouchableHighlight, View, ListView, Image, TextInput } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View, ListView, Image, TextInput, AsyncStorage } from 'react-native';
 import { connect } from 'react-redux';
-import {get_User_phone_no} from '../../lib/redux/basicAction'
+import { get_User_phone_no } from '../../lib/redux/basicAction'
 import { bindActionCreators } from 'redux'
 
- class LoginScreen extends Component {
+class LoginScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -19,13 +19,28 @@ import { bindActionCreators } from 'redux'
 
         }
         else {
-            this.props.get_User_phone_no(this.state.phonenumber)
-            this.props.navigation.navigate('Main', {
-                phoneNumber: this.state.phonenumber
-            });
+            AsyncStorage.setItem('userPhoneNumber', this.state.phonenumber, () => {
+                this.props.get_User_phone_no(this.state.phonenumber)
+                this.props.navigation.navigate('Main', {
+                    phoneNumber: this.state.phonenumber
+                });
+            })
+
         }
 
     }
+
+    componentWillMount() {
+        AsyncStorage.getItem('userPhoneNumber', (err, result) => {
+            if (result != null) {
+                this.props.get_User_phone_no(result)
+                this.props.navigation.navigate('Main', {
+                    phoneNumber: result
+                });
+            }
+        });
+    }
+
     render() {
         return (
             <View>
@@ -47,11 +62,11 @@ import { bindActionCreators } from 'redux'
 
 const mapDispatchToProps = dispatch => (
     bindActionCreators({
-      get_User_phone_no,
+        get_User_phone_no,
     }, dispatch)
-  );
+);
 
-  export default connect(null,mapDispatchToProps)(LoginScreen)
+export default connect(null, mapDispatchToProps)(LoginScreen)
 
 
 //nho check phonenumber null hoac ko phai so
